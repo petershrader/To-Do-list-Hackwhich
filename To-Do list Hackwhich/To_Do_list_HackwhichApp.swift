6592b86f43b7663ac029c2dfbc8e3cb250fd0fa5
Peter Shrader <pshrader@bsd220.org>
2022-01-10 10:39:39 -0600

    Initial Commit

---
+++ To-Do list Hackwhich/To_Do_list_HackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_list_HackwhichApp.swift
+//  To-Do list Hackwhich
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_list_HackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
