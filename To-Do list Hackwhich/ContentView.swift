e0d0ae7d6875bded50abf7b786ffbf43e216e4e0
Peter Shrader <pshrader@bsd220.org>
2022-01-12 10:41:21 -0600

    redesign list display

--- To-Do list Hackwhich/ContentView.swift
+++ To-Do list Hackwhich/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDoItems =
+        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List {
+                ForEach(toDoItems) { item in
+                    HStack {
+                        VStack {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    toDoItems.remove(atOffsets: indexSet)
+                    
+                })
+            }
+            .navigationBarTitle("To Do List", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +45,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
 }
